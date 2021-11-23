706fce1282da00d6398cf77fb9bc7d281da62109
rmdEp <rmdaw00@gmail.com>
2021-11-23 16:25:49 -0500

    feature file

---
+++ cucumber/src/test/java/com/rmdaw/cucumber/AppTest.java
@@ -0,0 +1,17 @@
+package com.rmdaw.cucumber;
+
+
+import org.junit.jupiter.api.*;
+
+
+class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    void shouldAnswerWithTrue()
+    {
+        Assertions.assertTrue( true );
+    }
+}
